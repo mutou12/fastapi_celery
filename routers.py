23114f7f538bccf4a54bc0b2dd03381ea25ada65
hekai <kai.he@nokia-sbell-huanuo.com>
2025-03-07 16:32:14 +0800

    添加 FastAPI 路由以处理任务请求，包括加法、乘法和求和功能

---
+++ routers.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter, HTTPException
+from task import add, mul, xsum
+from pydantic import BaseModel
+from app_factory import create_app
+from fastapi.encoders import jsonable_encoder
+import logging
+import os
+# 进行日志配置
+logging_config.setup_logging()
+logger = logging.getLogger(__name__)
+
+api_router = APIRouter()
+
+class AddTask(BaseModel):
+    x: int
+    y: int
+
+class MulTask(BaseModel):
+    x: int
+    y: int
+
+class XSumTask(BaseModel):
+    numbers: list[int]
+
+@api_router.get("/health-check")
+def health_check():
+    return {"status": "ok"}
+
+@api_router.post("/tasks/add")
+def run_add_task(task: AddTask):
+    task = add.apply_async((task.x, task.y))
+    return {"task_id": task.id}
+
+@api_router.post("/tasks/mul")
+def run_mul_task(task : MulTask):
+    task = mul.apply_async((task.x, task.y))
+    return {"task_id": task.id}
+
+@api_router.post("/tasks/xsum")
+def run_xsum_task(task: XSumTask):
+    task = xsum.apply_async((task.numbers,))
+    return {"task_id": task.id}
+
+@api_router.get("/tasks/{task_id}")
+def get_task_status(task_id: str):
+    app=create_app()
+    task = app.celery.AsyncResult(task_id)    
+    if task.state == 'PENDING':
+        return {"task_id": task.id, "state": task.state}
+    elif task.state != 'FAILURE':
+        return {"task_id": task.id, "state": task.state, "result": task.result}
+    else:
+        return {"task_id": task.id, "state": task.state, "error": str(task.info)}
